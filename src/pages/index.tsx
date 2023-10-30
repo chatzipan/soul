a2faa2a5026f0f78a365c922d51e07b1763a6dba
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2023-10-30 01:16:15 +0100

    feat: creates funny annimations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import Icon from "../assets/logo.svg";
 import styled from "styled-components";
+import latteArt from "../images/latte_art.png"; // Tell webpack this JS file uses this image
+import babka from "../images/babka.png"; // Tell webpack this JS file uses this image
 
 const Logo = styled(Icon)`
   width: 100%;
@@ -21,6 +23,86 @@ const Main = styled.main`
   color: white;
 `;
 
+const LatteArtWrapper = styled.div`
+  --size: 300px;
+  width: var(--size);
+  height: auto;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -1;
+  animation: x 23s linear infinite alternate, spin 24s linear infinite;
+
+  @keyframes x {
+    100% {
+      transform: translateX(calc(100vw - var(--size)));
+    }
+  }
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const BabkaWrapper = styled.div`
+  --size: 300px;
+  width: var(--size);
+  height: auto;
+  position: absolute;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -1;
+  animation: x 23s linear infinite alternate, spin 24s linear infinite;
+
+  @keyframes x {
+    100% {
+      transform: translateX(calc(100vw + var(--size)));
+    }
+  }
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const LatteArt = styled.img`
+  --size: 300px;
+  width: var(--size);
+  height: auto;
+  animation: y 17s linear infinite alternate;
+
+  @keyframes y {
+    100% {
+      transform: translateY(calc(100vh - 225px));
+    }
+  }
+`;
+
+const Babka = styled.img`
+  --size: 300px;
+  width: var(--size);
+  height: auto;
+  animation: y 17s linear infinite alternate;
+
+  @keyframes y {
+    100% {
+      transform: translateY(calc(100vh + 225px));
+    }
+  }
+`;
+
 const ComingSoon = styled.p`
   font-size: 2rem;
   font-weight: bold;
@@ -30,6 +112,12 @@ const ComingSoon = styled.p`
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Main>
+      <LatteArtWrapper>
+        <LatteArt src={latteArt} />
+      </LatteArtWrapper>
+      <BabkaWrapper>
+        <Babka src={babka} />
+      </BabkaWrapper>
       <Logo />
       <ComingSoon>coming soon...</ComingSoon>
     </Main>
