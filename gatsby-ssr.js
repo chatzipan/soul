58c55045d0987d25307234dd1632cc0c91756d98
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2023-11-05 18:37:24 +0100

    fix: build

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,20 @@
-import * as React from "react";
+const React = require("react");
+const { ThemeProvider } = require("styled-components");
+const Layout = require("./src/components/layout").default;
+const Theme = require("./src/theme").default;
 
-export const onRenderBody = ({ setHeadComponents }) => {
+exports.wrapPageElement = ({ element, props }) => {
+  // props provide same data to Layout as Page element will get
+
+  // including location, data, etc - you don't need to pass it
+  return <Layout {...props}>{element}</Layout>;
+};
+
+exports.wrapRootElement = ({ element }) => {
+  return <ThemeProvider theme={Theme}>{element}</ThemeProvider>;
+};
+
+exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <link
       rel='preload'
