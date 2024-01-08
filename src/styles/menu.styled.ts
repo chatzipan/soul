e0e5957efd26ec449605ae7f8a889c106b8818f3
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2024-01-08 23:13:37 +0100

    fix overflow

--- src/styles/menu.styled.ts
+++ src/styles/menu.styled.ts
@@ -24,6 +24,7 @@ export const Wrapper = styled.div`
     background: url(/svg/logo.svg) 0 0 repeat;
     background-size: 200px;
     transform: rotate(-30deg);
+    overflow: hidden;
 
     @media (max-width: ${({ theme }) => theme.sizes.laptop}) {
       top: -250%;
