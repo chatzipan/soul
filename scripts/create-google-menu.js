e81d4395d704cdfe39e4d702c072130ca64a7b13
Vasilis Chatzipanagiotis <vchatzipan@gmail.com>
2024-02-23 00:00:06 +0100

    feat. creates script for google menu

---
+++ scripts/create-google-menu.js
@@ -0,0 +1,100 @@
+const dotenv = require("dotenv");
+const fs = require("fs");
+dotenv.config({ path: __dirname + "/./../.env" });
+
+const { JWT } = require("google-auth-library");
+const keys = JSON.parse(fs.readFileSync("./elite.json", "utf8"));
+const client = new JWT({
+  email: keys.client_email,
+  key: keys.private_key,
+  scopes: [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/business.manage",
+  ],
+  subject: "v.chatzipanagiotis@soulcoffee.info",
+});
+
+async function saveGoogleMenu() {
+  const url = `https://dns.googleapis.com/dns/v1/projects/${keys.project_id}`;
+  await client.request({ url });
+
+  // After acquiring an access_token, you may want to check on the audience, expiration,
+  // or original scopes requested.  You can do that with the `getTokenInfo` method.
+  await client.getTokenInfo(client.credentials.access_token);
+
+  const accountsRequest = await fetch(
+    `https://mybusinessaccountmanagement.googleapis.com/v1/accounts?access_token=${client.credentials.access_token}`
+  );
+
+  const { accounts } = await accountsRequest.json();
+  const accountId = accounts[0].name;
+
+  const locationsRequest = await fetch(
+    `https://mybusinessbusinessinformation.googleapis.com/v1/${accountId}/locations?readMask=storeCode,regularHours,name,languageCode,title,phoneNumbers,categories,storefrontAddress,websiteUri,regularHours,specialHours,serviceArea,labels,adWordsLocationExtensions,latlng,openInfo,metadata,profile,relationshipData,moreHours&access_token=${client.credentials.access_token}`
+  );
+
+  const { locations } = await locationsRequest.json();
+  const locationId = locations[0].name;
+
+  const menu_endpoint = `https://mybusiness.googleapis.com/v4/${accountId}/${locationId}/foodMenus?access_token=${client.credentials.access_token}`;
+
+  const storedMenuResponse = await fetch(
+    "https://storage.googleapis.com/soulzuerich.ch/menu.json",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }
+  );
+
+  const storedMenu = await storedMenuResponse.json();
+
+  const googleMenu = {
+    menus: [
+      {
+        labels: {
+          displayName: "All Day Menu",
+          languageCode: "en",
+        },
+        sections: storedMenu.map((section) => ({
+          labels: {
+            displayName: section.name,
+            languageCode: "en",
+          },
+          items: section.entries.map((entry) => [
+            {
+              labels: {
+                displayName: entry.name,
+                languageCode: "en",
+              },
+              attributes: {
+                price: {
+                  currencyCode: "CHF",
+                  units: Math.floor(entry.price),
+                  nanos: (entry.price - Math.floor(entry.price)) * 1000000000,
+                },
+              },
+            },
+          ]),
+        })),
+      },
+    ],
+  };
+
+  try {
+    await fetch(menu_endpoint, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(googleMenu),
+    });
+    console.log("New Menu Saved!");
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+saveGoogleMenu();
--- scripts/upload-file.js
+++ scripts/upload-file.js
@@ -1,18 +1,15 @@
 // Imports the Google Cloud client library
 const { Storage } = require("@google-cloud/storage");
 
-// The ID of your GCS bucket
-const bucketName = "soulzuerich.ch";
-
-// The new ID for your GCS file
-const destFileName = "menu.json";
+const GCS_BUCKET_NAME = "soulzuerich.ch";
+const DEST_FILE_NAME = "menu.json";
 
 // Creates a client
 const storage = new Storage();
-const bucket = storage.bucket(bucketName);
+const bucket = storage.bucket(GCS_BUCKET_NAME);
 
 async function uploadFile(data) {
-  const file = bucket.file(destFileName);
+  const file = bucket.file(DEST_FILE_NAME);
   const contents = JSON.stringify(data);
   console.log("Uploading menu.json to GCS...");
 
@@ -20,7 +17,7 @@ async function uploadFile(data) {
     metadata: { contentType: "application/json", cacheControl: "no-cache" },
   });
 
-  await bucket.file(destFileName).makePublic();
+  await bucket.file(DEST_FILE_NAME).makePublic();
   return;
 }
 
