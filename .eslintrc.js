6290ff0714bacbb3bd524489a91c2a6733b156fa
Vasilis Chatzipanagiotis <vasilis.chatzipanagiotis@eraneos.com>
2025-01-19 11:53:43 +0200

    adds import rules on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,34 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: `react-app`,
+  plugins: ["import"],
+  extends: [
+    "react-app",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+    "eslint-config-prettier",
+  ],
   rules: {
     "prefer-const": "error",
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin", // e.g. node built-ins like fs or path
+          "external", // e.g. npm modules like react, firebase
+          "internal", // your internal code (alias-based imports)
+          ["parent", "sibling"], // imports from parent or sibling folders
+          "index", // index files
+          "object", // e.g. JSON files
+          "type", // Type imports
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc", // sort alphabetically
+          caseInsensitive: true, // case-insensitive sorting
+        },
+      },
+    ],
   },
 };
